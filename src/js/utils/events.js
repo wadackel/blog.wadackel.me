3f1b69c4686e459159d56a5981769202e8d8ae58
tsuyoshiwada <wadackel@gmail.com>
2018-03-11 18:02:56 +0900

    Add service worker

--- src/js/utils/events.js
+++ src/js/utils/events.js
@@ -1,13 +1,32 @@
-export function addEvent(el, event, listener) {
-  const events = event.split(',');
-  events.forEach((eventName) => {
-    el.addEventListener(eventName.trim(), listener, false);
-  });
-}
+const SUPPORTS_PASSIVE_OPTIONS = (() => {
+  let enablePassive = false;
 
-export function removeEvent(el, event, listener) {
+  try {
+    const opts = Object.defineProperty({}, 'passive', {
+      get() { // eslint-disable-line getter-return
+        enablePassive = true;
+      },
+    });
+    window.addEventListener('test', null, opts);
+  } catch (e) {} // eslint-disable-line no-empty
+
+  return enablePassive;
+})();
+
+export const addEvent = (el, event, listener, options) => {
   const events = event.split(',');
+  const opts = SUPPORTS_PASSIVE_OPTIONS ? options : options.capture;
+
   events.forEach((eventName) => {
-    el.removeEventListener(eventName.trim(), listener, false);
+    el.addEventListener(eventName.trim(), listener, opts);
   });
-}
+};
+
+export const removeEvent = (el, event, listener, options) => {
+  const events = event.split(',');
+  const opts = SUPPORTS_PASSIVE_OPTIONS ? options : options.capture;
+
+  events.forEach((eventName) => {
+    el.removeEventListener(eventName.trim(), listener, opts);
+  });
+};
--- src/js/utils/selectors.js
+++ src/js/utils/selectors.js
@@ -1,16 +1,16 @@
-export function $(selector, context = null) {
+export const $ = (selector, context = null) => {
   if (!selector) return undefined;
   return (context == null ? document : context).querySelector(selector);
-}
+};
 
-export function $$(selector, context = null) {
+export const $$ = (selector, context = null) => {
   if (!selector) return undefined;
   return (context == null ? document : context).querySelectorAll(selector);
-}
+};
 
-export function matches(el, selector) {
+export const matches = (el, selector) => {
   const m = (el.document || el.ownerDocument).querySelectorAll(selector);
   let i = m.length;
   while (--i >= 0 && m.item(i) !== el); // eslint-disable-line no-plusplus
   return i > -1;
-}
+};
